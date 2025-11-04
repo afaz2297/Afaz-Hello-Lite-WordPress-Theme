c0eff8f9d1064d16bdede920d5ac39a33acd7704
Md. Afaz Uddin <73947374+afaz2297@users.noreply.github.com>
2025-11-04 17:24:29 +0600

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
-aaaaa
+/**
+ * Afaz Hello Lite - Main JS
+ * Author: Afaz Alif
+ * GitHub: https://github.com/afaz2297
+ * Website: https://afazpro.com
+ * Version: 1.0.0
+ *
+ * This is a minimal JavaScript file for the theme.
+ * Add your custom scripts below.
+ */
+
+(function($) {
+    'use strict';
+
+    // Example: Document ready
+    $(document).ready(function() {
+        console.log('Afaz Hello Lite theme loaded successfully.');
+    });
+
+})(jQuery);
